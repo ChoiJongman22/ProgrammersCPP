fd7f9175a16558712910822b8c5081cce3cacd37
Jong-Hun-Choi <whdgjs3131@naver.com>
2020-10-14 20:08:24 +0900

    npos 기억하자

--- 멀쩡한 사각형.cpp
+++ 멀쩡한 사각형.cpp
@@ -3,17 +3,23 @@
 using namespace std;
 
 
-long long solution(int w, int h) {
-    long long answer = 1;
-    long long a=0;//최대공약수
-    for (int i = w < h ? w : h; i > 0; i--) {
-        if (w % i == 0 && h % i == 0) {
+size_t solution(int w, int h) {
+    size_t answer = 1;
+    size_t a=0;//최대공약수
+    //int i = 0;
+    /*if (w < h) w = i;
+    else h = i;*/
+    
+    //이 문제가 최대공약수로 풀어야하는 이유는 8*12를 하기전에 2*3부터 그려봐야 한다.
+
+    for (int i=w < h ? w : h; i > 0; i--) {//작은 값을 받는다. 사각형을 세로로 세우기 위해서
+        if (w % i == 0 && h % i == 0) { //예외처리할 때 w와 i가 0인 경우는 생각하지 않는다.
              a = i;
-             break;
+             break; //최대공약수 나오면 한다.
         }
     }
 
-    long long sum = (long long)w * (long long)h;
+    size_t sum = (size_t)w * (size_t)h;
     answer = sum - (a * (w / a + (h / a )- 1));
    
     
--- 스킬트리.cpp
+++ 스킬트리.cpp
@@ -17,34 +17,57 @@ using namespace std;
 //    return answer;
 //} 원래는 3중포문을 이용해서 문제 풀려고 했다.
 
-int solution(string skill, vector<string> skill_trees) {
-    int answer = 0;
-    map<char, int> sk;
-    for (int i = 0; i < skill.length(); i++) {
-        sk[skill[i]] = i + 1;
-    }
+//int solution(string skill, vector<string> skill_trees) {
+//    int answer = 0;
+//    map<char, int> sk;
+//    for (int i = 0; i < skill.length(); i++) {
+//        sk[skill[i]] = i + 1;
+//    }
+//
+//    for (auto skill_tree : skill_trees) {
+//        int cnt = 1;
+//        bool check = true;
+//        for (int j = 0; j < skill_tree.length(); j++) {
+//            if (sk[skill_tree[j]] > cnt) {
+//                check = false;
+//                break;
+//            }
+//            else if (sk[skill_tree[j]] == cnt) {
+//                cnt++;
+//            }
+//        }
+//        if (check) {
+//            answer++;
+//        }
+//    }
+//
+//    return answer;
+//}
 
-    for (auto skill_tree : skill_trees) {
-        int cnt = 1;
-        bool check = true;
-        for (int j = 0; j < skill_tree.length(); j++) {
-            if (sk[skill_tree[j]] > cnt) {
-                check = false;
-                break;
+
+int solution2(string skill, vector<string> skill_trees) {
+    int answer = 0;
+    int skillNum;
+    bool check;
+    for (int i = 0; i < skill_trees.size(); i++) {
+        check = true;
+        skillNum = 0;
+        for (int j = 0; j < skill_trees[i].size(); j++) {
+            if (skill.find(skill_trees[i][j]) == string::npos) {
+                continue;
             }
-            else if (sk[skill_tree[j]] == cnt) {
-                cnt++;
+            if (skill.find(skill_trees[i][j]) == skillNum) {
+                skillNum++;
+                continue;
             }
+            check = false;
+            break;
         }
-        if (check) {
-            answer++;
-        }
+        if (check == true) answer++;
     }
 
     return answer;
 }
-
-
 
 int main() {
     cout << solution("CBD", { "BACDE","CBADF","AECB","BDA" });
